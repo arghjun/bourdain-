8a0fccfa37bdbb61d58d5f7428a1b02ad88c3d87
Arjun Duraivenkatesh <dvarjun23@gmail.com>
2020-09-08 15:43:11 -0700

    test

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: "mongodb+srv://test:arjun@cluster0.naihp.mongodb.net/test1?retryWrites=true&w=majority",
+  secretOrKey: "arjun",
+  userRoles: ["user", "manager"]
+};
